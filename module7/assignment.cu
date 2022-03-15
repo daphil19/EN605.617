b6d5e01537905c4c4c9d90436dd9bcc0b435181c
Davd Phillips <daphillips19@gmail.com>
2022-03-14 22:42:32 -0400

    module all done

--- module7/assignment.cu
+++ module7/assignment.cu
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "operations.cuh"
 
 static const int RANDOM_RANGE = 4;
 
-typedef struct {
+// struct for handling input and output arrays for a given operation
+typedef struct
+{
     unsigned int *firstInput;
     unsigned int *secondInput;
     unsigned int *output;
@@ -11,81 +13,88 @@ typedef struct {
 
 __host__ cudaEvent_t get_time(void)
 {
-	cudaEvent_t time;
-	cudaEventCreate(&time);
-	cudaEventRecord(time);
-	return time;
+    cudaEvent_t time;
+    cudaEventCreate(&time);
+    cudaEventRecord(time);
+    return time;
 }
 
-__host__ void print_delta(cudaEvent_t start, cudaEvent_t stop) {
+// print the delta based on the provided start and stop events
+__host__ void print_delta(cudaEvent_t start, cudaEvent_t stop)
+{
     cudaEventSynchronize(stop);
 
-	float delta = 0;
-	cudaEventElapsedTime(&delta, start, stop);
+    float delta = 0;
+    cudaEventElapsedTime(&delta, start, stop);
     printf("%f\n", delta);
 }
 
-__host__ OPERATION_ARRAYS_T initialize_operation_arrays(size_t dataSizeBytes) {
+// allocates **GPU** based arrays for an operation
+__host__ OPERATION_ARRAYS_T initialize_operation_arrays(size_t dataSizeBytes)
+{
     OPERATION_ARRAYS_T ops;
-    cudaMalloc((void**)&ops.firstInput, dataSizeBytes);
-    cudaMalloc((void**)&ops.secondInput, dataSizeBytes);
-    cudaMalloc((void**)&ops.output, dataSizeBytes);
+    cudaMalloc((void **)&ops.firstInput, dataSizeBytes);
+    cudaMalloc((void **)&ops.secondInput, dataSizeBytes);
+    cudaMalloc((void **)&ops.output, dataSizeBytes);
 
     return ops;
 }
 
-__host__ void free_operation_arrays(OPERATION_ARRAYS_T ops) {
+// frees the **GPU** based arrays for an operation
+__host__ void free_operation_arrays(OPERATION_ARRAYS_T ops)
+{
     cudaFree(ops.firstInput);
     cudaFree(ops.secondInput);
     cudaFree(ops.output);
 }
 
-
-__host__ void printResults(OPERATION_ARRAYS_T op, int size, char operation) {
-    for (int i = 0; i < size; i++) {
+// pretty-prints the results of a given set of operations inputs and output
+__host__ void printResults(OPERATION_ARRAYS_T op, int size, char operation)
+{
+    for (int i = 0; i < size; i++)
+    {
         printf("%d %c %d = %d\n", op.firstInput[i], operation, op.secondInput[i], op.output[i]);
     }
 }
 
 int main(int argc, char **argv)
 {
-	// read command line arguments
-	unsigned int totalThreads = (1 << 20);
-	unsigned int blockSize = 256;
+    // read command line arguments
+    unsigned int totalThreads = (1 << 20);
+    unsigned int blockSize = 256;
 
-	if (argc >= 2)
-	{
-		totalThreads = atoi(argv[1]);
-	}
-	else
-	{
-		printf("Using default total threads %d\n", totalThreads);
-	}
-	if (argc >= 3)
-	{
-		blockSize = atoi(argv[2]);
-	}
-	else
-	{
-		printf("Using default block size %d\n", blockSize);
-	}
+    if (argc >= 2)
+    {
+        totalThreads = atoi(argv[1]);
+    }
+    else
+    {
+        printf("Using default total threads %d\n", totalThreads);
+    }
+    if (argc >= 3)
+    {
+        blockSize = atoi(argv[2]);
+    }
+    else
+    {
+        printf("Using default block size %d\n", blockSize);
+    }
     // "quiet" flag. If provided, only the timings will be printed to terminal
-	bool quiet = argc >= 4 && strncmp(argv[3], "--quiet", 7) == 0;
+    bool quiet = argc >= 4 && strncmp(argv[3], "--quiet", 7) == 0;
 
+    unsigned int numBlocks = totalThreads / blockSize;
 
-	unsigned int numBlocks = totalThreads / blockSize;
+    // validate command line arguments
+    if (totalThreads % blockSize != 0)
+    {
+        ++numBlocks;
+        totalThreads = numBlocks * blockSize;
 
-	// validate command line arguments
-	if (totalThreads % blockSize != 0)
-	{
-		++numBlocks;
-		totalThreads = numBlocks * blockSize;
+        printf("Warning: Total thread count is not evenly divisible by the block size\n");
+        printf("The total number of threads will be rounded up to %d\n", totalThreads);
+    }
 
-		printf("Warning: Total thread count is not evenly divisible by the block size\n");
-		printf("The total number of threads will be rounded up to %d\n", totalThreads);
-	}
-
-	size_t dataSizeBytes = sizeof(unsigned int) * totalThreads;
+    size_t dataSizeBytes = sizeof(unsigned int) * totalThreads;
 
     // Initialize streams
     cudaStream_t streamAdd, streamSub, streamMul, streamMod;
@@ -95,7 +104,6 @@ int main(int argc, char **argv)
     cudaStreamCreate(&streamMod);
 
     // define and allocate arryas
-    // TODO putting all of this into an array may make the most sense...
     unsigned int *firstInputCpu, *secondInputCpu, *outputCpu;
     unsigned int *firstInputGpu, *secondInputGpu, *outputGpu; // non-streamed processing
 
@@ -108,13 +116,13 @@ int main(int argc, char **argv)
     OPERATION_ARRAYS_T mulOps = initialize_operation_arrays(dataSizeBytes);
     OPERATION_ARRAYS_T modOps = initialize_operation_arrays(dataSizeBytes);
 
-    cudaMalloc((void**)&firstInputGpu, dataSizeBytes);
-    cudaMalloc((void**)&secondInputGpu, dataSizeBytes);
-    cudaMalloc((void**)&outputGpu, dataSizeBytes);
-
+    cudaMalloc((void **)&firstInputGpu, dataSizeBytes);
+    cudaMalloc((void **)&secondInputGpu, dataSizeBytes);
+    cudaMalloc((void **)&outputGpu, dataSizeBytes);
 
     // initialize array inputs
-    for (int i = 0; i < totalThreads; i++) {
+    for (int i = 0; i < totalThreads; i++)
+    {
         firstInputCpu[i] = i;
         secondInputCpu[i] = rand() % RANDOM_RANGE;
     }
@@ -123,7 +131,6 @@ int main(int argc, char **argv)
     cudaEvent_t streamStart = get_time();
 
     // copy data onto gpu
-    // TODO apparantly we async copy to the stream and then call the kernel
     cudaMemcpyAsync(addOps.firstInput, firstInputCpu, dataSizeBytes, cudaMemcpyHostToDevice, streamAdd);
     cudaMemcpyAsync(addOps.secondInput, secondInputCpu, dataSizeBytes, cudaMemcpyHostToDevice, streamAdd);
 
@@ -135,7 +142,6 @@ int main(int argc, char **argv)
 
     cudaMemcpyAsync(modOps.firstInput, firstInputCpu, dataSizeBytes, cudaMemcpyHostToDevice, streamMod);
     cudaMemcpyAsync(modOps.secondInput, secondInputCpu, dataSizeBytes, cudaMemcpyHostToDevice, streamMod);
-
 
     // block before we process, just to make sure the copy is done in time
     cudaStreamSynchronize(streamAdd);
@@ -156,13 +162,14 @@ int main(int argc, char **argv)
 
     cudaEvent_t streamStop = get_time();
 
-    if (!quiet) {
+    if (!quiet)
+    {
         OPERATION_ARRAYS_T host_outputs = {
             firstInputCpu,
             secondInputCpu,
-            outputCpu
-        };
+            outputCpu};
         cudaMemcpyAsync(host_outputs.output, addOps.output, dataSizeBytes, cudaMemcpyHostToDevice, streamAdd);
+        // block to make sure the copy is done correctly
         cudaStreamSynchronize(streamAdd);
         printResults(host_outputs, totalThreads, '+');
 
@@ -203,7 +210,6 @@ int main(int argc, char **argv)
     cudaEvent_t syncStop = get_time();
     printf("synchronous runtime: ");
     print_delta(syncStart, syncStop);
-
 
     cudaFreeHost(firstInputCpu);
     cudaFreeHost(secondInputCpu);
