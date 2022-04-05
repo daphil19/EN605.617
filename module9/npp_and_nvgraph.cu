87a82bff4e033352b6070151339e0117bd99b00b
Davd Phillips <daphillips19@gmail.com>
2022-04-05 01:27:10 +0000

    base code working now and pretty printing

--- module9/npp_and_nvgraph.cu
+++ module9/npp_and_nvgraph.cu
@@ -1,45 +1,8 @@
-// #include <string>
-// #include <iostream>
-// #include <fstream>
+///////////////////////////////////////////////////////////////////////////////
+// Portions of this source code were adapted from both the nvgraph_Pagerank and
+// boxFilterNPP examples found in this repo
+///////////////////////////////////////////////////////////////////////////////
 
-// #include <npp.h>
-
-// int main(int argc, char const *argv[])
-// {
-//     // TODO get from cli args
-//     std::string filename;
-
-//     std::ifstream infile(filename.data(), std::ifstream::in);
-
-//     if (!infile.good()) {
-//         std::cout << "unable to open file: <" << filename.data() << ">" << std::endl;
-//     }
-
-//     std::string outFilename = filename;
-
-//     std::string::size_type dot = outFilename.rfind('.');
-
-//     if (dot != std::string::npos)
-//     {
-//         outFilename = outFilename.substr(0, dot);
-//     }
-
-//     // TODO rename the extension!
-//     outFilename += "_boxFilter.pgm";
-
-
-//     // TODO we need an image handle
-//     // npp:
-
-//     return EXIT_SUCCESS;
-// }
-
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#  define WINDOWS_LEAN_AND_MEAN
-#  define NOMINMAX
-#  include <windows.h>
-#  pragma warning(disable:4819)
-#endif
 
 #include <ImagesCPU.h>
 #include <ImagesNPP.h>
@@ -57,6 +20,24 @@
 
 #include <helper_string.h>
 #include <helper_cuda.h>
+
+__host__ cudaEvent_t get_time(void)
+{
+    cudaEvent_t time;
+    cudaEventCreate(&time);
+    cudaEventRecord(time);
+    return time;
+}
+
+// print the delta based on the provided start and stop events
+__host__ void print_delta(cudaEvent_t start, cudaEvent_t stop)
+{
+    cudaEventSynchronize(stop);
+
+    float delta = 0;
+    cudaEventElapsedTime(&delta, start, stop);
+    printf("%f\n", delta);
+}
 
 inline int cudaDeviceInit(int argc, const char **argv)
 {
@@ -98,8 +79,148 @@ bool printfNPPinfo(int argc, char *argv[])
     return bVal;
 }
 
-void perform_nvgraph() {
-    printf("Performing nvgraph...");
+void perform_npp_operation(int argc, char* argv[]) {
+    try
+    {
+        std::string sFilename;
+        char *filePath;
+
+        cudaDeviceInit(argc, (const char **)argv);
+
+        if (printfNPPinfo(argc, argv) == false)
+        {
+            exit(EXIT_SUCCESS);
+        }
+
+        if (checkCmdLineFlag(argc, (const char **)argv, "input"))
+        {
+            getCmdLineArgumentString(argc, (const char **)argv, "input", &filePath);
+        }
+        else
+        {
+            filePath = sdkFindFilePath("Lena.pgm", argv[0]);
+        }
+
+        if (filePath)
+        {
+            sFilename = filePath;
+        }
+        else
+        {
+            sFilename = "Lena.pgm";
+        }
+
+
+        double imageScaling;
+ 
+        if (argc >= 2) {
+            imageScaling = atof(argv[1]);
+            std::cout << "Image will be scaled by a factor of " << imageScaling << " in both directions." << std::endl;
+        } else {
+            imageScaling = 2;
+            std::cout << "No image scaling argument provided. Image will be scaled by a default factor of " << imageScaling << std::endl;
+        }
+
+        // if we specify the filename at the command line, then we only test sFilename[0].
+        int file_errors = 0;
+        std::ifstream infile(sFilename.data(), std::ifstream::in);
+
+        if (infile.good())
+        {
+            std::cout << "boxFilterNPP opened: <" << sFilename.data() << "> successfully!" << std::endl;
+            file_errors = 0;
+            infile.close();
+        }
+        else
+        {
+            std::cout << "boxFilterNPP unable to open: <" << sFilename.data() << ">" << std::endl;
+            file_errors++;
+            infile.close();
+        }
+
+        if (file_errors > 0)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        std::string sResultFilename = sFilename;
+
+        std::string::size_type dot = sResultFilename.rfind('.');
+
+        if (dot != std::string::npos)
+        {
+            sResultFilename = sResultFilename.substr(0, dot);
+        }
+
+        sResultFilename += "_resized.pgm";
+
+        if (checkCmdLineFlag(argc, (const char **)argv, "output"))
+        {
+            char *outputFilePath;
+            getCmdLineArgumentString(argc, (const char **)argv, "output", &outputFilePath);
+            sResultFilename = outputFilePath;
+        }
+
+        // declare a host image object for an 8-bit grayscale image
+        npp::ImageCPU_8u_C1 oHostSrc;
+        // load gray-scale image from disk
+        npp::loadImage(sFilename, oHostSrc);
+        // declare a device image and copy construct from the host image,
+        // i.e. upload host to device
+        npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
+
+        NppiSize oSrcSize = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
+
+
+        NppiRect oSrcROI = {0, 0, oDeviceSrc.width(), oDeviceSrc.height()};
+        // allocate device image of appropriately reduced size
+        npp::ImageNPP_8u_C1 oDeviceDst(oDeviceSrc.width() * imageScaling, oDeviceSrc.height() * imageScaling);
+        std::cout << "done" << std::endl;
+
+        NppiSize dstROISize = {oDeviceDst.width(), oDeviceDst.height()};
+
+        NPP_CHECK_NPP (
+            nppiResize_8u_C1R(
+                oDeviceSrc.data(), 
+                oSrcSize, 
+                oDeviceSrc.pitch(), 
+                oSrcROI,
+                oDeviceDst.data(),
+                oDeviceDst.pitch(),
+                dstROISize,
+                imageScaling,
+                imageScaling,
+                NPPI_INTER_CUBIC
+            )
+        );
+
+        // declare a host image for the result
+        npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
+        // and copy the device result data into it
+        oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
+
+        saveImage(sResultFilename, oHostDst);
+        std::cout << "Saved image: " << sResultFilename << std::endl;
+    }
+    catch (npp::Exception &rException)
+    {
+        std::cerr << "Program error! The following exception occurred: \n";
+        std::cerr << rException << std::endl;
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+    catch (...)
+    {
+        std::cerr << "Program error! An unknow type of exception occurred. \n";
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+}
+
+void perform_nvgraph_operation() {
+    printf("Performing nvgraph operation...\n");
     const size_t  num_vertices = 6, num_edges = 10, vertex_numsets = 1, edge_numsets = 1;
     int i, *destination_offsets, *source_indices;
     float *weights;
@@ -181,7 +302,6 @@ void perform_nvgraph() {
     nvgraphDestroyGraphDescr(handle, graph);
     nvgraphDestroy(handle);
 
-
     delete[] destination_offsets;
     delete[] source_indices;
     delete[] weights;
@@ -190,209 +310,26 @@ void perform_nvgraph() {
     delete topology;
 }
 
-void check(nvgraphStatus_t status) {
-    if (status != NVGRAPH_STATUS_SUCCESS) {
-        printf("ERROR : %d\n",status);
-        exit(0);
-    }
-}
-
 int main(int argc, char *argv[])
 {
     printf("%s Starting...\n\n", argv[0]);
 
-    try
-    {
-        std::string sFilename;
-        char *filePath;
 
-        cudaDeviceInit(argc, (const char **)argv);
+    cudaEvent_t nppStart = get_time();
+    perform_npp_operation(argc, argv);
+    cudaEvent_t nppStop = get_time();
 
-        if (printfNPPinfo(argc, argv) == false)
-        {
-            exit(EXIT_SUCCESS);
-        }
-
-        if (checkCmdLineFlag(argc, (const char **)argv, "input"))
-        {
-            getCmdLineArgumentString(argc, (const char **)argv, "input", &filePath);
-        }
-        else
-        {
-            filePath = sdkFindFilePath("Lena.pgm", argv[0]);
-        }
-
-        if (filePath)
-        {
-            sFilename = filePath;
-        }
-        else
-        {
-            sFilename = "Lena.pgm";
-        }
-
-        // if we specify the filename at the command line, then we only test sFilename[0].
-        int file_errors = 0;
-        std::ifstream infile(sFilename.data(), std::ifstream::in);
-
-        if (infile.good())
-        {
-            std::cout << "boxFilterNPP opened: <" << sFilename.data() << "> successfully!" << std::endl;
-            file_errors = 0;
-            infile.close();
-        }
-        else
-        {
-            std::cout << "boxFilterNPP unable to open: <" << sFilename.data() << ">" << std::endl;
-            file_errors++;
-            infile.close();
-        }
-
-        if (file_errors > 0)
-        {
-            exit(EXIT_FAILURE);
-        }
-
-        std::string sResultFilename = sFilename;
-
-        std::string::size_type dot = sResultFilename.rfind('.');
-
-        if (dot != std::string::npos)
-        {
-            sResultFilename = sResultFilename.substr(0, dot);
-        }
-
-        sResultFilename += "_resized.pgm";
-
-        if (checkCmdLineFlag(argc, (const char **)argv, "output"))
-        {
-            char *outputFilePath;
-            getCmdLineArgumentString(argc, (const char **)argv, "output", &outputFilePath);
-            sResultFilename = outputFilePath;
-        }
-
-        // declare a host image object for an 8-bit grayscale image
-        npp::ImageCPU_8u_C1 oHostSrc;
-        // load gray-scale image from disk
-        npp::loadImage(sFilename, oHostSrc);
-        // declare a device image and copy construct from the host image,
-        // i.e. upload host to device
-        npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
-
-        // create struct with box-filter mask size
-        // NppiSize oMaskSize = {5, 5};
-
-        NppiSize oSrcSize = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
-        // NppiPoint oSrcOffset = {0, 0};
+    cudaEvent_t nvgraphStart = get_time();
+    perform_nvgraph_operation();
+    cudaEvent_t nvgraphStop = get_time();
 
 
-        double imageScaling = 2;
+    std::cout << "NPP Operation: ";
+    print_delta(nppStart, nppStop);
 
-        // create struct with ROI size
-        // NppiSize oSizeROI = {(int)oDeviceSrc.width() , (int)oDeviceSrc.height() };
-        NppiRect oSrcROI = {0, 0, oDeviceSrc.width(), oDeviceSrc.height()};
-        // allocate device image of appropriately reduced size
-        // std::cout << "crating dst " << oSrcROI.width * imageScaling << " " << oSrcROI.height * imageScaling << std::endl;
-        npp::ImageNPP_8u_C1 oDeviceDst(oDeviceSrc.width() * imageScaling, oDeviceSrc.height() * imageScaling);
-        std::cout << "done" << std::endl;
-        // set anchor point inside the mask to (oMaskSize.width / 2, oMaskSize.height / 2)
-        // It should round down when odd
-        // NppiPoint oAnchor = {oMaskSize.width / 2, oMaskSize.height / 2};
+    std::cout << "nvGRAPH Operation: ";
+    print_delta(nvgraphStart, nvgraphStop);
 
-        NppiSize dstROISize = {oDeviceDst.width(), oDeviceDst.height()};
-
-        /*NppStatus nppiResize_8u_C1R(
-            const Npp8u *pSrc, 
-            NppiSize oSrcSize, 
-            int nSrcStep, 
-            NppiRect oSrcROI, 
-            Npp8u *pDst, 
-            int nDstStep, 
-            NppiSize dstROISize, 
-            double nXFactor, 
-            double nYFactor, 
-            int eInterpolation
-        )*/
-
-        // nppiResize_8u_C1R(
-        //     oDeviceSrc.data(), 
-        //     oSrcSize, 
-        //     oDeviceSrc.pitch(), 
-        //     oSizeROI,
-        //     oDeviceDst.data(),
-        //     oDeviceDst.pitch(),
-        //     dstROISize,
-        //     imageScaling,
-        //     imageScaling,
-        //     NPPI_INTER_CUBIC
-        // );
-
-        /*NppStatus nppiFilterBoxBorder_8u_C1R(
-            const Npp8u *pSrc,
-            Npp32s nSrcStep, 
-            NppiSize oSrcSize,
-            NppiPoint oSrcOffset, 
-            Npp8u *pDst, 
-            Npp32s nDstStep, 
-            NppiSize oSizeROI, 
-            NppiSize oMaskSize, 
-            NppiPoint oAnchor, 
-            NppiBorderType eBorderType
-        )*/
-
-
-        // run box filter
-        // NPP_CHECK_NPP (
-        //                    nppiFilterBoxBorder_8u_C1R(oDeviceSrc.data(), oDeviceSrc.pitch(),
-        //                                               oSrcSize, oSrcOffset,
-        //                                               oDeviceDst.data(), oDeviceDst.pitch(),
-        //                                               oSizeROI, oMaskSize, oAnchor, NPP_BORDER_REPLICATE) );
-
-        // std::cout << oDeviceSrc.width() << std::endl <<  oDeviceSrc.height() << std::endl;
-
-        NPP_CHECK_NPP (
-            nppiResize_8u_C1R(
-                oDeviceSrc.data(), 
-                oSrcSize, 
-                oDeviceSrc.pitch(), 
-                oSrcROI,
-                oDeviceDst.data(),
-                oDeviceDst.pitch(),
-                dstROISize,
-                imageScaling,
-                imageScaling,
-                NPPI_INTER_CUBIC
-            )
-        );
-
-        // declare a host image for the result
-        npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
-        // and copy the device result data into it
-        oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
-
-        saveImage(sResultFilename, oHostDst);
-        std::cout << "Saved image: " << sResultFilename << std::endl;
-
-        // exit(EXIT_SUCCESS);
-    }
-    catch (npp::Exception &rException)
-    {
-        std::cerr << "Program error! The following exception occurred: \n";
-        std::cerr << rException << std::endl;
-        std::cerr << "Aborting." << std::endl;
-
-        exit(EXIT_FAILURE);
-    }
-    catch (...)
-    {
-        std::cerr << "Program error! An unknow type of exception occurred. \n";
-        std::cerr << "Aborting." << std::endl;
-
-        exit(EXIT_FAILURE);
-        return -1;
-    }
-
-    perform_nvgraph();
 
     return EXIT_SUCCESS;
 }
--- module9/thrust.cu
+++ module9/thrust.cu
@@ -9,43 +9,78 @@ template<
 typename ThrustVectorType,
 typename BinaryFunction
 >
-void compute_and_print(ThrustVectorType *first, ThrustVectorType *second, BinaryFunction op, char opStr) {
+void compute_and_print(ThrustVectorType *first, ThrustVectorType *second, BinaryFunction op, char opStr, bool quiet) {
     ThrustVectorType result(first->size());
 
     thrust::transform(first->begin(), first->end(), second->begin(), result.begin(), op);
 
-    for (int i = 0; i < first->size(); i++) {
-        std::cout << (*first)[i] << " " << opStr << " " << (*second)[i] << " = " << result[i] << std::endl;
+    if (!quiet) {
+        for (int i = 0; i < first->size(); i++) {
+            std::cout << (*first)[i] << " " << opStr << " " << (*second)[i] << " = " << result[i] << std::endl;
+        }
     }
 }
 
 template<
 typename ThrustVectorType
 >
-void perform_operations(ThrustVectorType *first, ThrustVectorType *second) {
-    std::cout << "Add: " << std::endl;
-    compute_and_print(first, second, thrust::plus<int>(), '+');
+void perform_operations(ThrustVectorType *first, ThrustVectorType *second, bool quiet) {
+    if (!quiet) {
+        std::cout << "Add: " << std::endl;
+    }
+    compute_and_print(first, second, thrust::plus<int>(), '+', quiet);
+    if (!quiet) {
     std::cout << "Substract: " << std::endl;
-    compute_and_print(first, second, thrust::minus<int>(), '-');
+    }
+    compute_and_print(first, second, thrust::minus<int>(), '-', quiet);
+        if (!quiet) {
     std::cout << "Multiply: " << std::endl;
-    compute_and_print(first, second, thrust::multiplies<int>(), '*');
+        }
+    compute_and_print(first, second, thrust::multiplies<int>(), '*', quiet);
+        if (!quiet) {
     std::cout << "Modulo: " << std::endl;
-    compute_and_print(first, second, thrust::modulus<int>(), '%');
+        }
+    compute_and_print(first, second, thrust::modulus<int>(), '%', quiet);
+}
+
+__host__ cudaEvent_t get_time(void)
+{
+    cudaEvent_t time;
+    cudaEventCreate(&time);
+    cudaEventRecord(time);
+    return time;
+}
+
+// print the delta based on the provided start and stop events
+__host__ void print_delta(cudaEvent_t start, cudaEvent_t stop)
+{
+    cudaEventSynchronize(stop);
+
+    float delta = 0;
+    cudaEventElapsedTime(&delta, start, stop);
+    printf("%f\n", delta);
 }
 
 int main(int argc, char const *argv[])
 {
 
-    // TODO args!
 
     size_t vector_size = 10;
+    if (argc >= 2) {
+        vector_size = atoi(argv[1]);
+    } else {
+        vector_size = 10;
+        std::cout << "No vector size argument provided. A default of " << vector_size << " will be used." << std::endl;
+    }
+
+    // "quiet" flag. If provided, only the timings will be printed to terminal
+    bool quiet = argc >= 3 && strncmp(argv[2], "--quiet", 7) == 0;
+
 
     /* code */
     thrust::device_vector<int> firstInputGpu(vector_size);
 
     thrust::sequence(firstInputGpu.begin(), firstInputGpu.end());
-
-    // thrust::device_vector<int> secondInput()
 
     // TODO instead of generating random number this way, we could look into
     // ways to generate the random numbers via curand and pass that to the
@@ -53,10 +88,13 @@ int main(int argc, char const *argv[])
     thrust::device_vector<int> secondInputGpu(vector_size);
 
     for (int i = 0; i < vector_size; i++) {
-        secondInputGpu[i] = rand() % 10;
+        secondInputGpu[i] = (rand() % 10) + 1; // gog a floating point exception with 0 for mod, so don't allow that as an option
     }
 
-    perform_operations(&firstInputGpu, &secondInputGpu);
+    cudaEvent_t device_start = get_time();
+    perform_operations(&firstInputGpu, &secondInputGpu, quiet);
+    cudaEvent_t device_end = get_time();
+
 
     thrust::host_vector<int> firstInputCpu(vector_size);
     thrust::host_vector<int> secondInputCpu(vector_size);
@@ -64,8 +102,14 @@ int main(int argc, char const *argv[])
     thrust::copy(firstInputGpu.begin(), firstInputGpu.end(), firstInputCpu.begin());
     thrust::copy(secondInputGpu.begin(), secondInputGpu.end(), secondInputCpu.begin());
 
-    perform_operations(&firstInputCpu, &secondInputCpu);
+    cudaEvent_t host_start = get_time();
+    perform_operations(&firstInputCpu, &secondInputCpu, quiet);
+    cudaEvent_t host_end = get_time();
 
+    std::cout << "GPU: ";
+    print_delta(device_start, device_end);
+    std::cout << "CPU: ";
+    print_delta(host_start, host_end);
 
     return EXIT_SUCCESS;
 }
