0de8bed1ac9af33991ff0fce8330cd0c182dca70
Davd Phillips <daphillips19@gmail.com>
2022-04-23 13:01:17 -0400

    begin work for module 12

---
+++ module12/assignment.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "info.hpp"
+
+#define NUM_BUFFER_ELEMENTS 16
+
+// TODO better convention!
+#define SUB_WINDOW_SIZE 2
+
+// Function to check and handle OpenCL errors
+inline void 
+checkErr(cl_int err, const char * name)
+{
+    if (err != CL_SUCCESS) {
+        std::cerr << "ERROR: " <<  name << " (" << err << ")" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+///
+//	main() for simple buffer and sub-buffer example
+//
+int main(int argc, char** argv)
+{
+    cl_int errNum;
+    cl_uint numPlatforms;
+    cl_uint numDevices;
+    cl_platform_id * platformIDs;
+    cl_device_id * deviceIDs;
+    cl_context context;
+    cl_program program;
+    std::vector<cl_kernel> kernels;
+    std::vector<cl_command_queue> queues;
+    std::vector<cl_mem> buffers;
+    int * inputOutput;
+
+    int platform = 0;
+
+    // First, select an OpenCL platform to run on.  
+    errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
+    checkErr( 
+        (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+        "clGetPlatformIDs"); 
+ 
+    platformIDs = (cl_platform_id *)alloca(
+            sizeof(cl_platform_id) * numPlatforms);
+
+    std::cout << "Number of platforms: \t" << numPlatforms << std::endl; 
+
+    errNum = clGetPlatformIDs(numPlatforms, platformIDs, NULL);
+    checkErr( 
+       (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+       "clGetPlatformIDs");
+
+    std::ifstream srcFile("simple.cl");
+    checkErr(srcFile.is_open() ? CL_SUCCESS : -1, "reading simple.cl");
+
+    std::string srcProg(
+        std::istreambuf_iterator<char>(srcFile),
+        (std::istreambuf_iterator<char>()));
+
+    const char * src = srcProg.c_str();
+    size_t length = srcProg.length();
+
+    deviceIDs = NULL;
+    DisplayPlatformInfo(
+        platformIDs[platform], 
+        CL_PLATFORM_VENDOR, 
+        "CL_PLATFORM_VENDOR");
+
+    errNum = clGetDeviceIDs(
+        platformIDs[platform], 
+        CL_DEVICE_TYPE_ALL, 
+        0,
+        NULL,
+        &numDevices);
+    if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
+    {
+        checkErr(errNum, "clGetDeviceIDs");
+    }       
+
+    deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
+    errNum = clGetDeviceIDs(
+        platformIDs[platform],
+        CL_DEVICE_TYPE_ALL,
+        numDevices, 
+        &deviceIDs[0], 
+        NULL);
+    checkErr(errNum, "clGetDeviceIDs");
+
+    cl_context_properties contextProperties[] =
+    {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)platformIDs[platform],
+        0
+    };
+
+    context = clCreateContext(
+        contextProperties, 
+        numDevices,
+        deviceIDs, 
+        NULL,
+        NULL, 
+        &errNum);
+    checkErr(errNum, "clCreateContext");
+
+    // Create program from source
+    program = clCreateProgramWithSource(
+        context, 
+        1, 
+        &src, 
+        &length, 
+        &errNum);
+    checkErr(errNum, "clCreateProgramWithSource");
+
+    // Build program
+    errNum = clBuildProgram(
+        program,
+        numDevices,
+        deviceIDs,
+        "-I.",
+        NULL,
+        NULL);
+    if (errNum != CL_SUCCESS) 
+    {
+        // Determine the reason for the error
+        char buildLog[16384];
+        clGetProgramBuildInfo(
+            program, 
+            deviceIDs[0], 
+            CL_PROGRAM_BUILD_LOG,
+            sizeof(buildLog), 
+            buildLog, 
+            NULL);
+
+            std::cerr << "Error in OpenCL C source: " << std::endl;
+            std::cerr << buildLog;
+            checkErr(errNum, "clBuildProgram");
+    }
+
+    std::cout << NUM_BUFFER_ELEMENTS * numDevices << std::endl;
+    inputOutput = new int[NUM_BUFFER_ELEMENTS * numDevices];
+    for (unsigned int i = 0; i < NUM_BUFFER_ELEMENTS * numDevices; i++)
+    {
+        inputOutput[i] = i;
+    }
+
+    // create a single buffer to cover all the input data
+    cl_mem main_buffer = clCreateBuffer(
+        context,
+        CL_MEM_READ_WRITE,
+        sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+        NULL,
+        &errNum);
+    checkErr(errNum, "clCreateBuffer");
+
+    // TODO so here is where we would create the sub buffers    
+    for (unsigned int i = 0; i < 4; i++) {
+        // here, we create sub-buffers that each take a portion of the main buffer
+                cl_buffer_region region = 
+            {
+                4 * i * sizeof(int), 
+                4 * sizeof(int)
+            };
+        cl_mem buffer = clCreateSubBuffer(
+            main_buffer,
+            CL_MEM_READ_WRITE,
+            CL_BUFFER_CREATE_TYPE_REGION,
+            &region,
+            &errNum);
+        checkErr(errNum, "clCreateSubBuffer");
+
+        buffers.push_back(buffer);
+
+    }
+
+    std::cout << "Program completed successfully" << std::endl;
+
+    return 0;
+}
---
+++ module12/filter.cl
@@ -0,0 +1,6 @@
+__kernel void filter(__global int* buffer) {
+    size_t id = get_global_id(0);
+    size_t lid = get_local_id(0);
+
+    
+}
